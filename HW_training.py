5ef6ab894198afcfdbd88ad510d830b29912563a
Dguzeppe-IT <Dguzepppe@gmail.com>
2025-05-01 02:15:52 +0300

    Add files via upload

---
+++ HW_training.py
@@ -0,0 +1,25 @@
+my_heigt = 175
+print(my_heigt)
+
+my_name = "Алексей"
+my_name = "Алексей Пузаков"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
